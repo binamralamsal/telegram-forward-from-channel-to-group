a16eeb2bfe7ba14eddbd88f7c0b717b79234f120
binamralamsal <binamralamsal@gmail.com>
2024-08-01 18:50:52 +0545

    fix ver

--- api/app.ts
+++ api/app.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 
 import { Bot, webhookCallback } from "grammy";
-import express from "express";
+import express, { type Request, type Response } from "express";
 
 const { BOT_TOKEN, CHANNEL_ID, GROUP_ID } = process.env;
 
@@ -31,7 +31,7 @@ const app = express();
 app.use(express.json());
 app.use(webhookCallback(bot, "express"));
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Bot is running!");
 });
 
